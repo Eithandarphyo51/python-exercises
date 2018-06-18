2c1e67dbc22b06fe61eb392ea1b1f1dc8115e314
Eithandarphyo51 <eithandarprogram@gmail.com>
2018-06-18 11:20:19 +0630

    ex14.py

--- ex14.py
+++ ex14.py
@@ -1,12 +1,12 @@
 from sys import argv
 
-script, user_name = agrv
+script, user_name = argv
 prompt = '> '
 
 print (f"Hi {user_name}, I'm the {script} script.")
 print ("I'd like to ask you a few questions.")
 print (f"Do you like me {user_name}?")
-like = input(prompt)
+likes = input(prompt)
 
 print (f"wher do you live {user_name}?")
 lives = input (prompt)
@@ -15,7 +15,7 @@ print (f"what kind of computer do you have?")
 computer = input(prompt)
 
 print(f"""
-        Alright, so you said {like} about liking me.
+        Alright, so you said {likes} about liking me.
         You live in {lives}. Not sure where that is.
         And you have a {computer} computer. Nice.
         """)

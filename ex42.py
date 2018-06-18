cc487b7bc5e44c92271074b7b8f67faf1f352640
Eithandarphyo51 <eithandarprogram@gmail.com>
2018-06-18 22:39:14 +0630

    update ex42.py

--- ex42.py
+++ ex42.py
@@ -1,8 +1,11 @@
 class Animal(object):
     pass
+
+
 class Dog(Animal):
 
     def __init__(self, name):
+
         self.name = name
 
 class Cat(Animal):
@@ -19,6 +22,7 @@ class Person(object):
 
         self.pet = None
 
+
 class Employee(Person):
 
     def __inti__(self, name, salary):
@@ -27,29 +31,42 @@ class Employee(Person):
 
         self.salary = salary
 
+
 class Fish(object):
     pass
+
 
 class Salmon(Fish):
     pass
 
+
 class Halibut(Fish):
     pass
 
+
+
 rover = Dog("Rover")
+
 
 satan = Cat("Satan")
 
+
 mary = Person("Mary")
+
 
 mary.pet = satan
 
+
 frank = Employee("Frank", 120000)
+
 
 frank.pet = rover
 
+
 flipper = Fish()
 
+
 crouse = Salmon()
+
 
 harry = Halibut()
